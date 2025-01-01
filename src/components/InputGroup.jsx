d24ecc3c8122fd0c2de68c582fa08a4945d751c4
Irvan Castro <castroirvan@gmail.com>
2025-01-01 22:52:32 +0800

    add name and onChange attribute to input

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,8 +1,8 @@
-export default function InputGroup({ label, type, id }) {
+export default function InputGroup({ label, type, id, name, onChange}) {
     return (
       <div className="input-group">
         <label htmlFor={id}>{label}</label>
-        <input type={type} id={id} />
+        <input type={type} id={id} name={name} onChange={onChange}/>
       </div>
     );
 }
