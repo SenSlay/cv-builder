8ca746a9f955a52a147ecc0029837bfa988cd5b4
Irvan Castro <castroirvan@gmail.com>
2025-01-01 22:51:14 +0800

    feat: dynamically update cv-header on input change
    - Added a formData object state that holds the personal info
    - created a handleInputChange function to pass on as handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ import InputGroup from './components/InputGroup'
 
 
 function App() {
+  const [formData, setFormData] = useState({
+    fullName: 'Irvan Castro',
+    email: 'email@gmail.com',
+    phoneNumber: '+63 123 456 7890',
+    address: 'Dubai, UAE',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <>
       <div className='edit-container'>
@@ -14,10 +30,12 @@ function App() {
 
         <div className='personal-details edit-section '>
           <h2>Personal Details</h2>
-          <InputGroup label="Full Name" type="text" id="full-name" />
-          <InputGroup label="Email" type="text" id="email" />
-          <InputGroup label="Phone Number" type="text" id="phone-number" />
-          <InputGroup label="Address" type="text" id="address" />
+          <div className="input-container">
+            <InputGroup label="Full Name" type="text" id="full-name" name="fullName" value={formData.fullName} onChange={handleInputChange} />
+            <InputGroup label="Email" type="text" id="email" name="email" value={formData.email} onChange={handleInputChange} />
+            <InputGroup label="Phone Number" type="text" id="phone-number" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange}/>
+            <InputGroup label="Address" type="text" id="address" name="address" value={formData.address} onChange={handleInputChange}  />
+          </div>
         </div>
         
         <div className='edit-section '>
@@ -31,7 +49,23 @@ function App() {
         </div>
       </div>
       <div className='cv-container'>
-        Resume
+        <div className='cv-header'>
+          <h1>{formData.fullName}</h1>
+          <div className='contact-info'>
+            <div>
+              <i className="fa-solid fa-envelope"></i>
+              <p>{formData.email}</p>
+            </div>
+            <div>
+              <i className="fa-solid fa-phone"></i>
+              <p>{formData.phoneNumber}</p>
+            </div>
+            <div>
+              <i className="fa-solid fa-location-dot"></i>
+              <p>{formData.address}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
