4336ccbf033698b879c58628e1ff1340460f07b8
Irvan Castro <castroirvan@gmail.com>
2025-02-07 18:08:55 +0800

    create variable initialNewExperience to match changes for handleAddItem

--- src/components/ExperienceEdit.jsx
+++ src/components/ExperienceEdit.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react'
 import InputGroup from './InputGroup';
 
-export default function ExperienceEdit({ experienceList, showForm, mode, formIndex, handleShowForm, handleCloseForm, handleCancelForm, handleInputChange, handleNewInputChange, handleAddItem, handleDeleteItem }) {
-    const [newExperience, setNewExperience] = useState({
-      company: '',
-      position: '',
-      'start-date': '',
-      'end-date': '',
-      description: ''
-    });
+export default function ExperienceEdit({ list, showForm, mode, formIndex, handleShowForm, handleCloseForm, handleCancelForm, handleInputChange, handleNewInputChange, handleAddItem, handleDeleteItem }) {
+    const initialNewExperience = {
+        company: '',
+        position: '',
+        'start-date': '',
+        'end-date': '',
+        description: ''
+    };
+    const [newExperience, setNewExperience] = useState(initialNewExperience);
 
     return (
         <div className='experience-input edit-section'>
             <h2>Experience</h2>
             {!showForm && (
               <ul className='experience-container'>
-                {experienceList.map((experience, index) => (
+                {list.map((experience, index) => (
                     <li key={index}>
                         <p>{experience.company}</p>
                         <div>
@@ -27,14 +28,14 @@ export default function ExperienceEdit({ experienceList, showForm, mode, formInd
               </ul>
             )}
             {showForm && (
-                <form name='experience' onSubmit={mode === 'edit' ? null : (e) => handleAddItem(e, newExperience)} className='experience-form'>
-                    <InputGroup label='Company' type='text' id='company' name='company' value={mode === 'edit' ? experienceList[formIndex].company : newExperience.company} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
-                    <InputGroup label='Position' type='text' id='position' name='position' value={mode === 'edit' ? experienceList[formIndex].position : newExperience.position} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
+                <form name='experience' onSubmit={mode === 'edit' ? null : (e) => handleAddItem(e, newExperience, setNewExperience, initialNewExperience)} className='experience-form'>
+                    <InputGroup label='Company' type='text' id='company' name='company' value={mode === 'edit' ? list[formIndex].company : newExperience.company} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
+                    <InputGroup label='Position' type='text' id='position' name='position' value={mode === 'edit' ? list[formIndex].position : newExperience.position} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
                     <div className='start-end-container'>
-                        <InputGroup label='start-date' type='text' id='start-date' name='start-date' value={mode === 'edit' ? experienceList[formIndex]['start-date'] : newExperience['start-date']} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
-                        <InputGroup label='end-date' type='text' id='end-date' name='end-date' value={mode === 'edit' ? experienceList[formIndex]['end-date'] : newExperience['end-date']} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
+                        <InputGroup label='start-date' type='text' id='start-date' name='start-date' value={mode === 'edit' ? list[formIndex]['start-date'] : newExperience['start-date']} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
+                        <InputGroup label='end-date' type='text' id='end-date' name='end-date' value={mode === 'edit' ? list[formIndex]['end-date'] : newExperience['end-date']} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
                     </div>
-                    <InputGroup label='Description' type='text' id='dsecription' name='description' value={mode === 'edit' ? experienceList[formIndex].description : newExperience.description} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
+                    <InputGroup label='Description' type='text' id='dsecription' name='description' value={mode === 'edit' ? list[formIndex].description : newExperience.description} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newExperience, setNewExperience)}></InputGroup>
                     <div className='form-button-container'>
                         <button type='button' onClick={mode === 'edit' ? handleCancelForm : () => handleCloseForm('experience')} className='form-cancel'>Cancel</button>
                         <button type='submit' onClick={mode === 'edit' ? () => handleCloseForm('experience') : null} className='form-save'>{mode === 'edit' ? 'Save Changes' : 'Add Experience'}</button>
