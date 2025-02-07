2f1d679b8eb13bca43e14350ca80d76ce75099f8
Irvan Castro <castroirvan@gmail.com>
2025-02-07 18:17:03 +0800

    refactor to use of useFormLogic

--- src/components/EducationEdit.jsx
+++ src/components/EducationEdit.jsx
@@ -1,93 +1,38 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import InputGroup from './InputGroup';
 
-export default function EducationEdit({ educationList, setEducationList, showForm, setShowForm, handleCloseForm, handleAddEducation }) {
-    const [mode, setMode] = useState(null);
-    const [formIndex, setFormIndex] = useState(0);
-    const [newEducation, setNewEducation] = useState({
+export default function EducationEdit({ list, showForm, mode, formIndex, handleShowForm, handleCloseForm, handleCancelForm, handleInputChange, handleNewInputChange, handleAddItem, handleDeleteItem }) {
+    const initialNewEducation = {
         school: '',
         degree: '',
         'start-date': '',
         'end-date': '',
-    });
-    const initialDataRef = useRef(null);
-
-    // Handle show form
-    const handleShowForm = (mode, index) => {
-        if (mode === 'edit') {
-            initialDataRef.current = { ...educationList[index] };
-            setFormIndex(index)
-        }
-        setMode(mode)
-        setShowForm({
-            ...showForm,
-            'education': true
-        });
-    };
-
-    // Handle cancel form
-    const handleCancelForm = () => {
-        const list = [...educationList]
-        
-        // Reverse the changes by re-assigning the initial value
-        list[formIndex] = {
-            ...initialDataRef.current
-        }
-        setEducationList(list);
-        setShowForm(false)
-    }   
-
-    // Handle input change
-    const handleInputChange = (e) => {
-        const updatedList = [...educationList];
-        updatedList[formIndex] = {
-            ...updatedList[formIndex],
-            [e.target.name]: e.target.value,
-        };
-        setEducationList(updatedList);
     }
-
-    // Handle new item input
-    const handleNewInputChange = (e) => {
-        const { name, value } = e.target;
-    
-        setNewEducation({
-            ...newEducation,
-            [name]: value,
-        });
-    }
-
-    // Handle deleting an education item
-    const handleDeleteEducation = (index) => {
-        if (window.confirm("Are you sure you want to delete this education?")) {
-            const updatedEducationList = educationList.filter((_, i) => i !== index);
-            setEducationList(updatedEducationList);
-        }
-    };
+    const [newEducation, setNewEducation] = useState(initialNewEducation);
     
     return (
         <div className='education-input edit-section '>
             <h2>Education</h2>
-            {!showForm.education && (
+            {!showForm && (
               <ul className='educations-container'>
-                {educationList.map((education, index) => (
+                {list.map((education, index) => (
                     <li key={index}>
                         <p>{education.school}</p>
                         <div>
                             <button onClick={() => handleShowForm('edit', index)}><i className="fa-regular fa-pen-to-square"></i></button>
-                            <button onClick={() => handleDeleteEducation(index)}><i className="fa-solid fa-trash"></i></button>
+                            <button onClick={() => handleDeleteItem(index)}><i className="fa-solid fa-trash"></i></button>
                         </div>
                     </li> 
                 ))}
               </ul>
             )}
-            {showForm.education && (
-                <form name='education' className='education-form' onSubmit={mode === 'edit' ? null : (e) => handleAddEducation(e, educationList, newEducation, setEducationList)}>
-                    <InputGroup label='School' type='text' id='school' name='school' value={mode === 'edit' ? educationList[formIndex].school : newEducation.school} onChange={mode === 'edit' ? handleInputChange : handleNewInputChange} required={'required'}></InputGroup>
-                    <InputGroup label='Degree' type='text' id='degree' name='degree' value={mode === 'edit' ? educationList[formIndex].degree : newEducation.degree} onChange={mode === 'edit' ? handleInputChange : handleNewInputChange}></InputGroup>
+            {showForm && (
+                <form name='education' className='education-form' onSubmit={mode === 'edit' ? null : (e) => handleAddItem(e, newEducation, setNewEducation, initialNewEducation)}>
+                    <InputGroup label='School' type='text' id='school' name='school' value={mode === 'edit' ? list[formIndex].school : newEducation.school} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newEducation, setNewEducation)} required={'required'}></InputGroup>
+                    <InputGroup label='Degree' type='text' id='degree' name='degree' value={mode === 'edit' ? list[formIndex].degree : newEducation.degree} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newEducation, setNewEducation)}></InputGroup>
                     <div className='start-end-container'>
-                        <InputGroup label='start-date' type='text' id='start-date' name='start-date' value={mode === 'edit' ? educationList[formIndex]['start-date'] : newEducation['start-date']} onChange={mode === 'edit' ? handleInputChange : handleNewInputChange}></InputGroup>
-                        <InputGroup label='end-date' type='text' id='end-date' name='end-date' value={mode === 'edit' ? educationList[formIndex]['end-date'] : newEducation['end-date']} onChange={mode === 'edit' ? handleInputChange : handleNewInputChange}></InputGroup>
+                        <InputGroup label='start-date' type='text' id='start-date' name='start-date' value={mode === 'edit' ? list[formIndex]['start-date'] : newEducation['start-date']} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newEducation, setNewEducation)}></InputGroup>
+                        <InputGroup label='end-date' type='text' id='end-date' name='end-date' value={mode === 'edit' ? list[formIndex]['end-date'] : newEducation['end-date']} onChange={mode === 'edit' ? handleInputChange : (e) => handleNewInputChange(e, newEducation, setNewEducation)}></InputGroup>
                     </div>
                     <div className='form-button-container'>
                         <button type='button' onClick={mode === 'edit' ? handleCancelForm : () => handleCloseForm('education')} className='form-cancel'>Cancel</button>
@@ -95,7 +40,7 @@ export default function EducationEdit({ educationList, setEducationList, showFor
                     </div>
                 </form>
             )}
-            {!showForm.education && (
+            {!showForm && (
                 <button className='add-button' onClick={() => handleShowForm('add')}><i className="fa-solid fa-plus"></i>  Education</button>
             )}
         </div>
