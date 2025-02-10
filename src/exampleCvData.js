af0e385ca47ce3733f8eee75c9b9b95f2949ba0d
Irvan Castro <castroirvan@gmail.com>
2025-02-10 20:21:35 +0800

    place example data in another file

---
+++ src/exampleCvData.js
@@ -0,0 +1,61 @@
+// Example summary data
+const exampleSummary = {
+  description: 'Im the goat'
+}
+
+// Example personal details data
+const examplePersonalDetails = {
+    fullName: 'Irvan Castro',
+    email: 'email@gmail.com',
+    phoneNumber: '+63 123 456 7890',
+    address: 'Dubai, UAE',
+}
+
+// Example education data
+const exampleEducation = [
+  { 
+    school: 'Mapua University',
+    degree: 'Bachelor of Science in Computer Science',
+    'start-date': 'Aug, 2023',
+    'end-date': 'Present',
+  }
+]
+
+// Example experience data
+const exampleExperience = [
+  {
+    company: 'Web Developer Intern',
+    position: 'Company',
+    'start-date': 'Oct 23',
+    'end-date': 'Dec 10',
+    description: 'Created front-end components using React.'
+  },
+  {
+    company: 'ProbeCX Ph',
+    position: 'Customer Service Representative',
+    'start-date': 'Oct 10',
+    'end-date': 'Dec 10',
+    description: 'Handled customer inquiries. Deez nuts, damn daniel, skibidi sigma.'
+  },
+]
+
+// Example projects data 
+const exampleProject = [
+  {
+    name: 'CV Builder',
+    tools: 'React, Javascript, HTML, CSS',
+    'start-date': 'Jan 1',
+    'end-date': 'Present',
+    description: 'Created using React'
+  }
+]
+
+// Example skills data
+const exampleSkill = [
+  {
+    category: 'Coding Languages',
+    skills: 'Javascript, HTML/CSS, React, Python',
+  }
+]
+
+export { exampleSummary, examplePersonalDetails, exampleEducation, exampleExperience, exampleProject, exampleSkill }
