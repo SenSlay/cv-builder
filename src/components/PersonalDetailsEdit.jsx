ef0f3b4b1ef3b38d0d1e648cc1b3cd55a5d57e86
Irvan Castro <castroirvan@gmail.com>
2025-02-10 20:18:20 +0800

    refactor to use useFormLogic

--- src/components/PersonalDetailsEdit.jsx
+++ src/components/PersonalDetailsEdit.jsx
@@ -1,14 +1,14 @@
 import InputGroup from "./InputGroup"
 
-export default function PersonalDetailsEdit({ formData, handleInputChange}) {
-    return (
+export default function PersonalDetailsEdit({ data, handleInputChange}) {
+  return (
         <div className='edit-section '>
             <h2>Personal Details</h2>
             <div className="input-container">
-            <InputGroup label="Full Name" type="text" id="full-name" name="fullName" value={formData.fullName} onChange={handleInputChange} />
-            <InputGroup label="Email" type="text" id="email" name="email" value={formData.email} onChange={handleInputChange} />
-            <InputGroup label="Phone Number" type="text" id="phone-number" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange}/>
-            <InputGroup label="Address" type="text" id="address" name="address" value={formData.address} onChange={handleInputChange}  />
+            <InputGroup label="Full Name" type="text" id="full-name" name="fullName" value={data.fullName} onChange={handleInputChange} />
+            <InputGroup label="Email" type="text" id="email" name="email" value={data.email} onChange={handleInputChange} />
+            <InputGroup label="Phone Number" type="text" id="phone-number" name="phoneNumber" value={data.phoneNumber} onChange={handleInputChange}/>
+            <InputGroup label="Address" type="text" id="address" name="address" value={data.address} onChange={handleInputChange}  />
             </div>
         </div>
     )
