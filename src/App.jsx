f711d44cef3e67f968293acb2547cf5baf964d7b
Irvan Castro <castroirvan@gmail.com>
2025-02-11 01:32:24 +0800

    feat: implement download cv function
    - Also made .cv-container into a component

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,17 @@ import ProjectsEdit from './components/ProjectsEdit';
 import SkillsEdit from './components/SkillsEdit';
 import useFormLogic from './utils/useFormLogic';
 import { exampleSummary, examplePersonalDetails, exampleEducation, exampleExperience, exampleProject, exampleSkill } from './exampleCvData';
+import { useRef} from "react";
+import { useReactToPrint } from "react-to-print";
+import CV from './components/CV';
 
 function App() {
-
+  const componentRef = useRef(null);
+  
+  const handlePrint = useReactToPrint({
+    contentRef: componentRef, 
+  });
+  
   // Summary section state and functions
   const {
     list: summary,
@@ -85,9 +93,12 @@ function App() {
   return (
     <>
       <div className='edit-container'>
-        <div>
+        <div className='control-buttons'>
           <button>Clear Resume</button>
-          <button>Load Example</button>
+          <button onClick={() => {
+            console.log("Print button clicked"); // Debug
+            handlePrint();
+          }}>Download CV</button>
         </div>
 
         <SummaryEdit
@@ -161,90 +172,15 @@ function App() {
           handleDeleteItem={handleSkillDeleteItem}
         ></SkillsEdit>
       </div>
-      <div className='cv-container'>
-        <div className='cv-header'>
-          <h1>{personalDetails.fullName}</h1>
-          <div className='contact-info'>
-            <div>
-              <i className="fa-solid fa-envelope"></i>
-              <p>{personalDetails.email}</p>
-            </div>
-            <div>
-              <i className="fa-solid fa-phone"></i>
-              <p>{personalDetails.phoneNumber}</p>
-            </div>
-            <div>
-              <i className="fa-solid fa-location-dot"></i>
-              <p>{personalDetails.address}</p>
-            </div>
-          </div>
-        </div>
-        <div className='summary-info cv-section'>
-          <h2>Summary</h2>
-          <p>{summary.description}</p>
-        </div>
-        <div className='education-info cv-section'>
-          <h2>Education</h2>
-          <ul>
-            {educationList.map((education, index) => (
-              <li key={index}>
-                <div>
-                  <h3>{education.school}</h3>
-                  <p>{education.degree}</p>
-                </div>
-                <p className='date-container'>
-                    {education['start-date']} - {education['end-date']}
-                </p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className='experience-info cv-section'>
-            <h2>Experience</h2>
-            <ul>
-              {experienceList.map((experience, index) => (
-                <li key={index}>
-                  <div>
-                    <h3>{experience.company}</h3>
-                    <h4>{experience.position}</h4>
-                    <p>{experience.description}</p>
-                  </div>
-                  <p className='date-container'>{experience['start-date']} - {experience['end-date']}</p>
-                </li>
-              ))}
-            </ul>
-        </div>
-        <div className="projects-info cv-section">
-          <h2>Projects</h2>
-          <ul>
-            {projectList.map((project, index) => (
-              <li key={index}>
-                <div>
-                  <div>
-                    <h3>{project.name}</h3>
-                    <p>| {project.tools}</p>
-                  </div>
-                <p>{project.description}</p>
-                </div>
-                <p className='date-container'>{project['start-date']} - {project['end-date']}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="skills-info cv-section">
-          <h2>Skills</h2>
-          <ul>
-            {skillList.map((skill, index) => (
-              <li key={index}>
-                <div>
-                  {skill.category && <p className='category-data'>{skill.category}:</p>}
-                  <p>{skill.skills}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <CV
+        ref={componentRef}
+        personalDetails={personalDetails}
+        summary={summary}
+        educationList={educationList}
+        experienceList={experienceList}
+        projectList={projectList}
+        skillList={skillList}
+      />
     </>
   )
 }
